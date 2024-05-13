09f2859f626f9fa76f2aaadafa9d54d5d7ebc08c
ChanThien3101 <chicanniemtin3101@gmail.com>
2024-05-13 12:25:27 +0700

    hu

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: 5000,
   MONGODB_URL: process.env.MONGODB_URL,
   CLOUD_NAME: process.env.CLOUD_NAME,
   CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
